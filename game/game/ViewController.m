546ba090d7b17bed9107c4980b7ee12ba5a6ba46
hanatang <jtang31@ucsc.edu>
2017-02-23 23:04:45 -0800

    add hints

--- game/game/ViewController.m
+++ game/game/ViewController.m
@@ -14,9 +14,6 @@
 
 @implementation ViewController
 
-
-
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
@@ -39,21 +36,23 @@
     [colorArray addObject:color2Array];
     [colorArray addObject:color3Array];
     
-    /*add 9*9*/
-    float l=(self.view.frame.size.width-8)/13;
+    /*add 9*9 cells*/
+    float l=(self.view.frame.size.width-8)/11;
     cellArray=[[NSMutableArray alloc] init];
     for(int i=0;i!=9;i++)
         for(int j=0;j!=9;j++)
         {
-            UITextField* cell=[[UITextField alloc] initWithFrame:CGRectMake(2*l+(l+1)*i, 150+(l+1)*j, l, l)];
+            UITextView* cell=[[UITextView alloc] initWithFrame:CGRectMake(l+(l+1)*j, 100+(l+1)*i, l, l)];
             UIColor *color=[[colorArray objectAtIndex:(i/3)] objectAtIndex:(j/3)];
             cell.layer.borderColor=color.CGColor;
-            cell.layer.borderWidth= 2.0f;
+            cell.layer.borderWidth=2.0f;
             cell.textColor=color;
             cell.keyboardType=UIKeyboardTypeNumberPad;
+            cell.font=[UIFont systemFontOfSize:18.0];
             cell.textAlignment=UITextAlignmentCenter;
             cell.delegate=self;
-            [cell addTarget:self action:@selector(textFieldDidChange:) forControlEvents:UIControlEventEditingChanged];
+           // [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(textViewDidChange:)name:@"UITextViewTextDidChangeNotification" object:cell];
+
             [cellArray addObject:cell];
             [self.view addSubview:cell];
         }
@@ -70,6 +69,7 @@
     [animation setTitle:@"ANIMATE" forState:UIControlStateNormal];
     animation.backgroundColor = [UIColor blackColor];
     [animation addTarget:self action:@selector(animation) forControlEvents:UIControlEventTouchUpInside];
+    //[animation addTarget:self action:@selector(try) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview: animation];
     
     
@@ -82,10 +82,10 @@
     
 }
 
--(void)textFieldDidChange:(UITextField *)textField{
-    NSString *s = textField.text;
+-(void)textViewDidChange:(UITextView *)textView{
+    NSString *s = textView.text;
     if(s.length>0)
-        textField.text = [s substringFromIndex:s.length-1];
+        textView.text = [s substringFromIndex:s.length-1];
 }
 
 -(void) reset{
@@ -93,9 +93,11 @@
     {
         for(int j=0;j!=9;j++)
         {
-            UITextField* cell=[cellArray objectAtIndex:j*9+i];
+            UITextView* cell=[cellArray objectAtIndex:i*9+j];
             cell.text=@"";
-            [cellArray replaceObjectAtIndex:j*9+i withObject:cell];
+            cell.font=[UIFont systemFontOfSize:18.0];
+            cell.contentInset = UIEdgeInsetsMake(0.f, 0.f, 0.f, 0.f);
+            [cellArray replaceObjectAtIndex:i*9+j withObject:cell];
             fix[i][j]=0;
         }
     }
@@ -103,8 +105,6 @@
 
 -(void) submit{
     animate=0;
-    //[self putNum:row andColumn:column andBlock:block andFix:fix];
-    
     /**********MARK************/
     NSThread * myThread = [[NSThread alloc]initWithTarget:self selector:@selector(start) object:nil];
     [myThread start];
@@ -114,8 +114,6 @@
 
 -(void) animation{
     animate=1;
-    //[self putNum:row andColumn:column andBlock:block andFix:fix];
-    
     /**********MARK************/
     NSThread * myThread = [[NSThread alloc]initWithTarget:self selector:@selector(start) object:nil];
     [myThread start];
@@ -127,21 +125,10 @@
 -(void) start{
     for(int i=0;i!=[cellArray count];i++)
     {
-        UITextField* cell=[cellArray objectAtIndex:i];
+        UITextView* cell=[cellArray objectAtIndex:i];
         fix[i/9][i%9]=cell.text.intValue;
+        Row[i/9][i%9]=cell.text.intValue;
     }
-    
-    int row [9][9];
-    int column [9][9];
-    int block [9][9];
-    
-    for(int i=0;i<9;i++)
-        for(int j=0;j<9;j++)
-        {
-            row[i][j]=fix[i][j];
-            column[j][i]=fix[i][j];
-            block[[self blockI:i andJ:j]][[self blockJ:i andJ:j]]=fix[i][j];
-        }
     
     /*verify the entries*/
     for(int i=0;i<9;i++)
@@ -149,166 +136,56 @@
         {
             if(fix[i][j]!=0)
             {
-                row[i][j]=0;
-                column[j][i]=0;
-                block[[self blockI:i andJ:j]][[self blockJ:i andJ:j]]=0;
-                if(![self checkNum:fix[i][j] andI:i andJ:j andRow:row andColumn:column andBlock:block]){
+                if(![self checkNum:fix[i][j] Row:i Column:j]){
                     
                     NSLog(@"Wrong entry");
                     return;
                 }
-                row[i][j]=fix[i][j];
-                column[j][i]=fix[i][j];
-                block[[self blockI:i andJ:j]][[self blockJ:i andJ:j]]=fix[i][j];
             }
         }
     
-    do{}while([self putFix:row andColumn: column andBlock:block andFix:fix]);[self putFix:row andColumn: column andBlock:block andFix:fix];
-    [self putNum:row andColumn:column andBlock:block andFix:fix];
+    do{}while([self putFix]);
+    [self putFix];
+    [self putNum];
 }
 
 
-//i j -> cubeI cubeJ
--(int)blockI:(int) i andJ:(int) j
-{
-    return ((i / 3)*3) + (j / 3);
-}
--(int)blockJ:(int) i andJ:(int) j
-{
-    return ((i % 3)*3) + (j % 3);
-}
-//chech if the number could be filled in
--(BOOL)checkNum:(int) n andI:(int) i andJ:(int) j andRow:(int[9][9]) row andColumn:(int[9][9]) column andBlock:(int[9][9]) block
-{
-    int result=0;
-    for(int k=0;k<9;k++)
-    {
-        if(n==row[i][k])
-            result=1;
-    }
-    for(int k=0;k<9;k++)
-    {
-        if(n==column[j][k])
-            result=1;
-    }
-    for(int k=0;k<9;k++)
-    {
-        if(n==block[[self blockI:i andJ:j]][k])
-            result=1;
-    }
-    
-    if(result==1)
-        return FALSE;
-    else
-        return TRUE;
-}
-//check if fixed number
--(BOOL)checkFix:(int) i andJ:(int) j andFix:(int[9][9]) fixN
-{
-    return (fixN[i][j]!=0);
-}
-
-//find final fixed
--(BOOL)checkRow:(int) n andI:(int) i andJ:(int) j andRow:(int[9][9])row andColumn:(int[9][9])column andBlock:(int[9][9])block andFix:(int[9][9])fixN
-{
-    for(int k=0;k<9;k++)
-    {
-        if([self checkFix:i andJ:k andFix:fixN]||k==j)
-            continue;
-        else if([self checkNum:n andI:i andJ:k andRow:row andColumn:column andBlock:block])
-            return FALSE;
-    }
-    return TRUE;
-}
--(BOOL)checkColumn:(int) n andI:(int) i andJ:(int) j andRow:(int[9][9])row andColumn:(int[9][9])column andBlock:(int[9][9])block andFix:(int[9][9])fixN
-{
-    for(int k=0;k<9;k++)
-    {
-        if([self checkFix:k andJ:j andFix:fixN]||k==i)
-            continue;
-        else if([self checkNum:n andI:k andJ:j andRow:row andColumn:column andBlock:block])
-            return FALSE;
-        
-    }
-    return TRUE;
-}
--(BOOL)checkBlock:(int) n andI:(int) i andJ:(int) j andRow:(int[9][9])row andColumn:(int[9][9])column andBlock:(int[9][9])block andFix:(int[9][9])fixN
-{
-    for(int k=0;k<9;k++)
-    {
-        if([self checkFix:([self blockI:i andJ:j]/3)*3+k/3 andJ:([self blockI:i andJ:j]%3)*3+k%3 andFix:fixN]||(([self blockI:i andJ:j]/3)*3+k/3==i && ([self blockI:i andJ:j]%3)*3+k%3 ==j))
-            continue;
-        else if([self checkNum:n andI:([self blockI:i andJ:j]/3)*3+k/3 andJ:([self blockI:i andJ:j]%3)*3+k%3 andRow:row andColumn:column andBlock:block])
-            return FALSE;
-    }
-    return TRUE;
-}
-
-
--(BOOL)putFix:(int[9][9])row andColumn:(int[9][9])column andBlock:(int[9][9])block andFix:(int[9][9])fixN
-{
+-(BOOL)putFix{
     int find=0;
+    int count=0;
+    int num=0;
     for(int i=0;i<9;i++)
         for(int j=0;j<9;j++)
         {
-            if([self checkFix:i andJ:j andFix:fixN])
+            if(fix[i][j]!=0)
                 continue;
-            else
+            else{
+                num=0;
+                count=0;
                 for(int n=1;n<=9;n++)
                 {
-                    
-                    if([self checkNum:n andI:i andJ:j andRow:row andColumn:column andBlock:block]&&([self checkRow:n andI:i andJ:j andRow:row andColumn:column andBlock:block andFix:fixN]||[self checkColumn:n andI:i andJ:j andRow:row andColumn:column andBlock:block andFix:fixN]||[self checkBlock:n andI:i andJ:j andRow:row andColumn:column andBlock:block andFix:fixN]))
-                    {
-                        row[i][j]=n;
-                        column[j][i]=n;
-                        block[[self blockI:i andJ:j]][[self blockJ:i andJ:j]]=n;
-                        fixN[i][j]=n;
-                        find=1;
-                        break;
+                    if([self checkNum:n Row:i Column:j]){
+                        count+=1;
+                        num=n;
                     }
-                    
                 }
+                if(count==1){
+                    fix[i][j]=num; //fixNum
+                    Row[i][j]=num;  //current filled
+                    find=1;
+                }
+            }
+            
         }
-    if (find==0)
-        return FALSE;
-    else
-        return TRUE;
+    return find==1;
 }
 
-
--(void)putFixShow:(int[9][9])row andColumn:(int[9][9])column andBlock:(int[9][9])block andFix:(int[9][9])fixN
-{
-    
-    for(int i=0;i<9;i++)
-        for(int j=0;j<9;j++)
-        {
-            if([self checkFix:i andJ:j andFix:fixN])
-                continue;
-            else
-                for(int n=1;n<=9;n++)
-                {
-                    
-                    if([self checkNum:n andI:i andJ:j andRow:row andColumn:column andBlock:block]&&([self checkRow:n andI:i andJ:j andRow:row andColumn:column andBlock:block andFix:fixN]||[self checkColumn:n andI:i andJ:j andRow:row andColumn:column andBlock:block andFix:fixN]||[self checkBlock:n andI:i andJ:j andRow:row andColumn:column andBlock:block andFix:fixN]))
-                    {
-                        row[i][j]=n;
-                        column[j][i]=n;
-                        block[[self blockI:i andJ:j]][[self blockJ:i andJ:j]]=n;
-                        fixN[i][j]=n;
-                        break;
-                    }
-                    
-                }
-        }
-}
-
-
--(void) putNum:(int[9][9])row andColumn:(int[9][9])column andBlock:(int[9][9])block andFix:(int[9][9])fixN
-{
+-(void) putNum{
     for(int i=0;i<9;i++)
     {
         for(int j=0;j<9;j++)
         {
-            if([self checkFix:i andJ:j andFix:fixN])
+            if(fix[i][j]!=0)
                 continue;
             else
             {
@@ -316,18 +193,11 @@
                 {
                     if(n<=9)
                     {
-                        if([self checkNum:n andI:i andJ:j andRow:row andColumn:column andBlock:block])
+                         if([self checkNum:n Row:i Column:j])
                         {
-                            row[i][j]=n;
-                            column[j][i]=n;
-                            block[[self blockI:i andJ:j]][[self blockJ:i andJ:j]]=n;
-                            
-                            /*******/
-                            if(animate==1)
-                            {
-                                for(int a=0;a!=9;a++)
-                                    for(int b=0;b!=9;b++)
-                                        Row[a][b]=row[a][b];
+                            Row[i][j]=n;
+                             /*******/
+                            if(animate==1){
                                 [self performSelectorOnMainThread:@selector(show) withObject:nil waitUntilDone:YES];
                                 [NSThread sleepForTimeInterval:0.1];
                             }
@@ -340,10 +210,8 @@
                     }
                     else
                     {
-                        row[i][j]=0;
-                        column[j][i]=0;
-                        block[[self blockI:i andJ:j]][[self blockJ:i andJ:j]]=0;
-                        int ti=0,tj=0;
+                        Row[i][j]=0;
+                         int ti=0,tj=0;
                         if(j==0)
                         {
                             ti=(i-1);
@@ -362,14 +230,13 @@
                                 NSLog(@"No Solution"); //no solution
                                 return;
                             }
-                            if(fixN[ti][tj]==0)
+                            if(fix[ti][tj]==0)
                             {
                                 i=ti;
                                 j=tj;
-                                n=row[i][j];
-                                row[i][j]=0;
-                                column[j][i]=0;
-                                block[[self blockI:i andJ:j]][[self blockJ:i andJ:j]]=0;
+                                n=Row[i][j];
+                                Row[i][j]=0;
+
                                 break;
                             }
                             if(tj==0)
@@ -390,9 +257,6 @@
     /*****************/
     if(animate==0)
     {
-        for(int a=0;a!=9;a++)
-            for(int b=0;b!=9;b++)
-                Row[a][b]=row[a][b];
         [self performSelectorOnMainThread:@selector(show) withObject:nil waitUntilDone:YES];
     }
 }
@@ -404,7 +268,7 @@
     {
         for(int j=0;j!=9;j++)
         {
-            UITextField* cell=[cellArray objectAtIndex:j*9+i ];
+            UITextView* cell=[cellArray objectAtIndex:j*9+i ];
             if(Row[j][i]==0) cell.text=@"";
             else cell.text=[NSString stringWithFormat:@"%d",Row[j][i]];
             if(fix[j][i]==Row[j][i]) cell.font = [UIFont boldSystemFontOfSize:18.0];
@@ -412,6 +276,66 @@
             [cellArray replaceObjectAtIndex:j*9+i withObject:cell];
         }
     }
+}
+
+/***************************************new version*********************************************/
+-(void) try{
+    for(int i=0;i!=[cellArray count];i++)
+    {
+        UITextView* cell=[cellArray objectAtIndex:i];
+        if([cell.text isEqualToString:@""]) Row[i/9][i%9]=0;
+        else Row[i/9][i%9]=cell.text.intValue;
+    }
+    [self showAvailableNums];
+}
+-(void)showAvailableNums{
+   
+    
+    for(int i=0;i!=9;i++){
+        for(int j=0;j!=9;j++){
+            if(Row[i][j]==0){
+                NSString* result=@"";
+                for(int n=1;n<=9;n++){
+                    if([self checkNum:n Row:i Column:j]){
+                        result=[NSString stringWithFormat:@"%@%d", result,n];
+                    }
+                    else{
+                        result=[NSString stringWithFormat:@"%@%@", result,@" "];
+                    }
+                    result=[NSString stringWithFormat:@"%@%@", result,@" "];
+                    if(n==3||n==6){
+                        result=[NSString stringWithFormat:@"%@%@", result,@"\n"];
+                    }
+                }
+                UITextView* cell=[cellArray objectAtIndex:i*9+j ];
+                //cell.contentInset = UIEdgeInsetsMake(-5.f, 0.f, 0.f, 0.f);
+                cell.font = [UIFont boldSystemFontOfSize:5.0];
+                cell.text=result;
+                NSLog(@"%@",result);
+            }
+        }
+    }
+}
+
+-(bool)checkNum:(int) n Row:(int) i Column:(int) j{
+    for(int k=0;k!=9;k++){
+        if(k!=j&&Row[i][k]==n){
+            return false;
+        }
+    }
+    for(int k=0;k!=9;k++){
+        if(k!=i&&Row[k][j]==n){
+            return false;
+        }
+    }
+    for(int a=0;a!=3;a++){
+        for(int b=0;b!=3;b++){
+            if(i/3*3+a!=i&&j/3*3+b!=j&&Row[i/3*3+a][j/3*3+b]==n){
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
 - (void)didReceiveMemoryWarning {
